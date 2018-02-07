e03b4974debf11bbd39831f5b1f415771454d39b
IsaacWeb <iantaro@mail.ru>
2018-02-07 13:51:42 -0800

    Dom play/pause checkpoint

--- scripts/album.js
+++ scripts/album.js
@@ -29,11 +29,31 @@ var albumMarconi = {
     ]
 };
 
+var albumLP = {
+    title: 'A Thousand Suns',
+    artist: 'Linkin Park',
+    label: 'RB',
+    year: '2010',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'The Requiem', duration: '2:01' },
+        { title: 'The Radiance', duration: '2:41' },
+        { title: 'Burning in the Skies', duration: '4:13'},
+        { title: 'Empty Spaces', duration: '4:14' },
+        { title: 'When They Come for Me', duration: '4:53'}
+    ]
+};
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -44,12 +64,6 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 
  var setCurrentAlbum = function(album) {
-
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -64,5 +78,48 @@ var createSongRow = function(songNumber, songName, songLength) {
  };
 
  window.onload = function() {
-     setCurrentAlbum(albumMarconi);
+     setCurrentAlbum(albumPicasso);
+
+ var setAlbums = [albumPicasso, albumMarconi, albumLP];
+     var index = 0;
+     albumImage.addEventListener("click", function(event) {
+       setCurrentAlbum(setAlbums[index]);
+     index++;
+     if (index == setAlbums.length) {
+       index = 0 ;
+     }
+   });
  };
+   var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+  window.onload = function() {
+      setCurrentAlbum(albumPicasso);
+
+      songListContainer.addEventListener('mouseover', function(event) {
+          console.log(event.target);
+      });
+ };
+
+
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+ var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+window.onload = function() {
+    setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event) {
+      if (event.target.parentElement.className === 'album-view-song-item') {
+           event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      }
+  });
+
+  for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Revert the content back to the number
+             // Selects first child element, which is the song-item-number element
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+   }
